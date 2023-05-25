caaedd7a89c46026d50188a3e096fd285568c695
Shrihari Magar <shriharimagar1@gmail.com>
2023-05-25 23:49:43 +0530

    Started with Admin Page

--- src/main/java/com/example/librarydbnew/LoginAdminController.java
+++ src/main/java/com/example/librarydbnew/LoginAdminController.java
@@ -1,7 +1,21 @@
 package com.example.librarydbnew;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginAdminController {
+    public void switchAdminPage(ActionEvent Action) throws IOException {
+        final var stage = new Stage();
+        final FXMLLoader fxmlLoader = new FXMLLoader(BookApplication.class.getResource("AdminPage.fxml"));
+        final Scene adminPage = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Admin Page");
+        stage.setScene(adminPage);
+        stage.show();
+    }
 }
